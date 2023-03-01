f5acdfa892236b28f4687ea71015bffb5f84a347
zakariayazdani-kiwi <zakaria.yazdani@kiwitech.com>
2023-03-01 17:00:56 +0530

    initial-setup-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import MovieCard from "./Components/MovieCard";
+import NavBar from "./Components/NavBar";
+import { data } from "./data";
 function App() {
   return (
-    <div>
-     <h1>Movie app</h1>
+    <div className="App">
+      <h1>Movie App</h1>
+      <NavBar />
+    <div className="main">
+      <div className="tabs">
+        <div className="tab">Movies</div>
+        <div className="tab">favourites</div>
+      </div>
+      <div className="list">
+        {
+          data.map((movie)=>(
+            <MovieCard movie={movie}/>
+          ))
+        }
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+     <div className="navbar">
+        <div className="search-container">
+            <input />
+            <button id="search-btn">Search</button>
+        </div>
+
+     </div>
+    );
+  }
+  
+  export default NavBar;
+  
